fd880267243a98c1ba81c2b7f9935f458501f01a
Guillem Gorgori Pérez <guillemgorgoriperez@MacBook-Air-de-Guillem.local>
2019-09-10 20:17:04 +0200

    first commit

---
+++ app/src/main/java/com/gorpe/pdf/MainActivity.java
@@ -0,0 +1,28 @@
+package com.gorpe.pdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, getString(R.string.admob_app_id));
+        MobileAds.initialize(this, getString(R.string.banner_id));
+        MobileAds.initialize(this, getString(R.string.interstitial_id));
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+    }
+}
